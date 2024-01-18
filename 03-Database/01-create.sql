0fb4f89220f519c746b2f7e1ee68ca1aca748312
unknown <ismael2095951@gmail.com>
2024-01-18 15:50:26 -0300

    comando sobre base de datos

---
+++ 03-Database/01-create.sql
@@ -0,0 +1,5 @@
+
+-- en MySQL, el concepto de base de datos y schema es igual, es el contenedor de todo (tablas)
+
+-- crea (CREATE) una base de datos (DATABASE) con el nombre test
+CREATE DATABASE test;
